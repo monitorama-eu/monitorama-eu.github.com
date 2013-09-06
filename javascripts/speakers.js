486dc3963661184b82a3b22a464d995d9a7f988d
obfuscurity <jason@dixongroup.net>
2013-09-05 23:59:14 -0400

    placeholder abstract elements

--- javascripts/speakers.js
+++ javascripts/speakers.js
@@ -4,6 +4,7 @@ var speakers = [
     "github": "detour1999",
     "twitter": "dylanr",
     "bio": 'Dylan Richard is the full-bearded technologist who, as Director for Engineering, quietly led the 40 engineers of Obama for America’s Technology team to victory in 2012. Previously, he helmed technical teams as Vice President of Engineering at skinnyCorp (home of threadless.com) and was a Senior Developer at Crate & Barrel. Dylan lives in Chicago, Illinois, with his patient wife, Sarah, two amazing sons, a cat, and a rotating stable of old diesel VWs. His possible favorite claim to fame is having his chest-length beard proclaimed “fierce” by President Barack Obama.',
+    "abstract": "",
     "hash": "4b1cdbcad65fbd94eec315104b9800cf"
   },
   {
@@ -11,6 +12,7 @@ var speakers = [
     "github": "Danese",
     "twitter": "DivaDanese",
     "bio": "Danese Cooper (born January 19, 1959) is an American programmer, computer scientist, and an advocate of open source software Cooper has held many leadership roles within the computer science sector. She has managed teams at Symantec and Apple Inc. and for six years served as Chief Open Source Evangelist for Sun Microsystems before leaving to serve as Senior Director for Open Source Strategies at Intel. She advised on open source policy to the R community while at REvolution Computing (now Revolution Analytics), and she served from February 2010 to July 2011 as Chief Technical Officer for the Wikimedia Foundation.  She currently runs a successful consultancy to companies wishing to pursue open source strategies, which has served the SETI Foundation, Harris Corporation and the Bill & Melinda Gates Foundation among other clients. She is a director on the boards of the Drupal Association, and the Open Source Hardware Association, a board advisor for Mozilla and Ushahidi, and has served since 2005 as a Member of the Apache Software Foundation. She was a board member for 10 years at Open Source Initiative.",
+    "abstract": "",
     "hash": "fa2a5801e4867dc2ad65363ecfa2f9ee"
   },
   {
@@ -18,6 +20,7 @@ var speakers = [
     "github": "pushmatrix",
     "twitter": "pushmatrix",
     "bio": "Daniel Beauchamp is the lead developer of Shopify Labs and an alumni of the Faculty of Engineering at the University of Ottawa. He is also one of the core members of Open Data Ottawa: a movement to push governments to make public data freely accessible to the public, and to make awesome mashups based on the data.",
+    "abstract": "",
     "hash": "c54e76d2a8908b68d73f73485a540855"
   },
   {
@@ -25,6 +28,7 @@ var speakers = [
     "github": "f3ew",
     "twitter": "f3ew",
     "bio": "Devdas Bhagat is a system and network administrator with over a decade of experience. His skillset ranges from small corporate environments with a few tens of users, to large scale ISP systems with a few million users. He has dabbled in email, DNS, monitoring, and database administration, along with the occasional bit of web server configuration, security stuff and network management (along with writing the occasional bit of code).<br /><br /> He is a regular speaker at conferences like SANOG, LCA and APRICOT. Devdas currently works at Booking.com, where he is responsible for the automation of the monitoring system, the Graphite setup, and the odd other bits and pieces of messaging infrastructure and DNS.",
+    "abstract": "",
     "hash": "c7e7bc7e9c13c425d05f7e0076b9900b",
   },
   {
@@ -32,6 +36,7 @@ var speakers = [
     "github": "kdaniels",
     "twitter": "beerops",
     "bio": "Katherine is an operations engineer at GameChanger.io, where she focuses on automation and monitoring while occasionally dabbling in beer-driven development. Before escaping to the world of operations at NYC-based startups, she spent a few years doing QA and systems engineering in the corporate world. She lives in Brooklyn with a perfectly reasonable number of cats, and in her spare time can often be found rock climbing or making hot sauce.",
+    "abstract": "",
     "hash": "dd0081cd62e25c796f50e1401132b19c"
   },
   {
@@ -39,6 +44,7 @@ var speakers = [
     "github": "morekid",
     "twitter": "morekid",
     "bio": "Daniele de Matteis was most recently involved with designing the colours, layout and user interaction with the new Server Density graphing on web and iOS. He's also worked on website design, logos, print and his personal projects including augmented reality iPad interactions.",
+    "abstract": "",
     "hash": "ca13887d38ae03906a68dfcc28738318"
   },
   {
@@ -46,6 +52,7 @@ var speakers = [
     "github": "octo",
     "twitter": "flocto",
     "bio": "Florian has been passionate about monitoring and statistics for years - his first open source projects were log analysers. He started the collectd project in 2005 while studying computer science in Erlangen, Germany. Florian is currently working as a Site Reliability Engineer for Google and still hacking on free software projects in his spare time.",
+    "abstract": "",
     "hash": "b2f1c54b9a6cdf618a033407a245afb4"
   },
   {
@@ -53,6 +60,7 @@ var speakers = [
     "github": "radu-gheorghe",
     "twitter": "radu0gheorghe",
     "bio": "Radu is a software engineer at Sematext, where he enjoys working on Logsene, a log analytics product. He is the co-author of <em>Elasticsearch in Action</em>. He also enjoys dancing the jive with his wife, and doing jam sessions with his son.",
+    "abstract": "",
     "hash": "2164d281261ff5f5a239a9ccd5bdcad4"
   },
   {
@@ -60,6 +68,7 @@ var speakers = [
     "github": "dgoodlad",
     "twitter": "dgoodlad",
     "bio": "David Goodlad is a Canadian developer living in Australia, hacking on ops and systems as operations lead for the Envato marketplaces. He has a terrible obsession with tools, and loves automating everything from tiny embedded systems driving hvac equipment all the way up to racks full of servers.",
+    "abstract": "",
     "hash": "f9336c3f47cfcde2389a4329d976c5b9"
   },
   {
@@ -67,6 +76,7 @@ var speakers = [
     "github": "gorsuch",
     "twitter": "michaelgorsuch",
     "bio": "Michael is an Oklahoma native making a name for himself in the world of lolcats and unicorns. He has worked alongside internet pioneers while innovating The Cloud at firms such as Fog Creek Software, Heroku and GitHub. His passions include weekend mushroom hunting, Volkswagens, and sipping picklebacks, although his family ranks a close fourth. It can never be said that Michael is not a fan of hard work, although he'd just as soon be lying in his hammock reading E.L. James novels.",
+    "abstract": "",
     "hash": "2c80e1b0837eec76d38d4f7fa1c15e87"
   },
   {
@@ -74,6 +84,7 @@ var speakers = [
     "github": "auxesis",
     "twitter": "auxesis",
     "bio": "Lindsay Holmwood is a engineering manager living in the Australian Blue Mountains. He runs a distributed infracoders team at Bulletproof that builds hassle free tools, and was responsible for ensuring 100% uptime for the 2010 + 2011 + 2012 Movember campaigns. In his spare time, Lindsay organises the monthly Sydney DevOps Meetups. He also won third place at the 1996 Sydney Royal Easter Show LEGO building competition.",
+    "abstract": "",
     "hash": "fad1e9ed293fc5b3ec7d4abdffeb636f"
   },
   {
@@ -81,6 +92,7 @@ var speakers = [
     "github": "jarkko",
     "twitter": "jarkko",
     "bio": "Jarkko Laine is a co-founder and jack of all trades at bearmetal.eu. He became one of the the early Ruby on Rails evangelists in Europe after semi-accidentally ending up body-doubling DHH in a Rails tutorial in EuroOSCON 2005. He's since written two books on the subject, <em>Unobtrusive Prototype</em> and <em>Beginning Ruby on Rails E-Commerce</em>.<br /><br />Jarkko spent the past five years as part of the team that built Wildfire App from scratch to serving large part of Fortune 50 companies before it was sold to Google last year. As an orienteer, he has an insatiable thirst to learn more about the human mind, body, and especially their co-operation. When not running aimlessly around the woods, Jarkko now helps founders build profitable web businesses as part of the Bear Metal team.",
+    "abstract": "",
     "hash": "82476266af9d460415d8f1fc16bb54ed"
   },
   {
@@ -88,6 +100,7 @@ var speakers = [
     "github": "reyjrar",
     "twitter": "reyjrar",
     "bio": "Brad Lhotsky programs Perl and throws large amounts of data into PostgreSQL databases and ElasticSearch indexes in search of meaning.  He often ponders why security is seen as a roadblock to development and operations while trying ever so hard to automate himself out of a job.  Brad has 14 years of experience in operations and development and is currently leading a small team at Booking.com making security more agile.",
+    "abstract": "",
     "hash": "8d96f5c273062cb617255e630fe0705c"
   },
   {
@@ -95,6 +108,7 @@ var speakers = [
     "github": "mmcgrana",
     "twitter": "mmcgrana",
     "bio": "Mark is CTO at Heroku, the cloud application platform, where he's responsible for technology strategy and architecture. In his role Mark draws on his experience as a cloud infrastructure engineer and product architect. Mark has been an active contributor to the open source community, including as the creator of the Ring web library and the <em>Go by Example</em> online book. He graduated from Bowdoin College with an A.B. in Mathematics and Computer Science.",
+    "abstract": "",
     "hash": "3423442e7b51da238e663b343169b756"
   },
   {
@@ -102,6 +116,7 @@ var speakers = [
     "github": "mihasya",
     "twitter": "mihasya",
     "bio": "Mikhail is a co-founder at Opsmatic, a startup dedicated to improving the tools available to operators at companies of all sizes. For inspiration and motivation, he draws on his experiences working with and around ops teams at Urban Airship, SimpleGeo, Flickr and Yahoo!. When not stressing out about computers, he's learning to surf, playing soccer, thinking of what else could be wall-mounted in his apartment, or just kicking back with good friends and good drink.",
+    "abstract": "",
     "hash": "127b5dbc702c58a40ee060044427ef1e"
   },
   {
@@ -109,6 +124,7 @@ var speakers = [
     "github": "ifesdjeen",
     "twitter": "ifesdjeen",
     "bio": "Zefops, gymops. Clojure / Ruby / C hacker. @ClojureWerkz core team. Data analysis & visualization nerd. Constantly thinking of new ways to make things more visible, predictable and stable.",
+    "abstract": "",
     "hash": "c1a634a6db1049a4b98b365f63a5fd97"
   },
   {
@@ -116,6 +132,7 @@ var speakers = [
     "github": "derfloh",
     "twitter": "derfloh",
     "bio": "Florian Pfeiffer made his first steps at Yahoo! as a developer on various European products. After six years he joined gutefrage.net where he worked in various teams and is currently Head of Data & Infrastructure. He likes the challenges of running & monitoring a high traffic website that has several releases a day.",
+    "abstract": "",
     "hash": "c4614ebbe33c9dbbb7c4e4b4b8c97ade"
   },
   {
@@ -123,6 +140,7 @@ var speakers = [
     "github": "pyr",
     "twitter": "pyr",
     "bio": "Principal Architect at https://exoscale.ch, the leading swiss public IAAS service. Recovering operations engineer now focused on building distributed systems, with a a knack for monitoring, visibility and instrumentation. Member of the OpenBSD and Collectd development teams.",
+    "abstract": "",
     "hash": "2fcc875f98607b3007909fe4be99160d"
   },
   {
@@ -130,6 +148,7 @@ var speakers = [
     "github": "garethr",
     "twitter": "garethr",
     "bio": "Gareth Rushgrove is a technical architect at the Government Digital Service, part of the UK Government. He is mainly interested in configuration management, infrastructure and platform as a service, automation and the whole devops community. He thinks when used well together these allow you to move really fast, even in tightly controlled environments like Government. When not working, Gareth can be found blogging over on morethanseven.net or uploading code to GitHub. He also curates the Devops Weekly newsletter and occasionally organises community events.",
+    "abstract": "",
     "hash": "98234c645fe8c935edc0fec0186d28b8"
   },
   {
@@ -137,6 +156,7 @@ var speakers = [
     "github": "postwait",
     "twitter": "postwait",
     "bio": "Theo Schlossnagle is a Founder and Principal at OmniTI where he designs and implements scalable solutions for highly trafficked sites and other clients in need of sound, scalable architectural engineering. He is the architect of the highly scalable Ecelerity mail transport agent. Theo is a participant in various open source communities including OpenSolaris, Linux, Apache, PostgreSQL, perl, and many others. He is a published author in the area of scalability and distributed systems as well as a veteran speaker in the open source conference circuit.<br /><br />Theo founded several successful startups as engineering focused organizations including: OmniTI, Circonus, Message Systems and Fontdeck.",
+    "abstract": "",
     "hash": "a23d0bf95d6eeb2d848512f198cad190"
   },
   {
@@ -144,6 +164,7 @@ var speakers = [
     "github": "ryandotsmith",
     "twitter": "ryandotsmith",
     "bio": "I spend my days solving problems in Heroku's distributed computing environment. In previous days, I studied math at the university of Missouri Kansas City.",
+    "abstract": "",
     "hash": "8964943d31146bd35d3d305ff940beca"
   },
   {
@@ -151,6 +172,7 @@ var speakers = [
     "github": "sparez",
     "twitter": "rmspagnolo",
     "bio": "Reza has been exploring for the past few years how IaaS and PaaS look like on the other side of the cloud. Before that, he was responsible for bringing ideas from paper to production for several startups of the web and mobile industries. Fusing development and operations together is the only approach to building systems that has ever made sense to him and he enjoys now taking the practice to the next level as an AWS engineer.",
+    "abstract": "",
     "hash": "885b20f047883867d98de21dce6c2e83"
   },
   {
@@ -158,6 +180,7 @@ var speakers = [
     "github": "astanway",
     "twitter": "abestanway",
     "bio": "Abe is a data engineer at Etsy and he lives in Brooklyn. He wrote Skyline, but more importantly, commitlogsfromlastnight.com - the premier software for monitoring curses in your commit logs. Previously, he monitored users at Chartbeat. He once wrote a service to monitor course openings for undergraduate class registration. He monitors Twitter at @abestanway.",
+    "abstract": "",
     "hash": "6601d82cf1b6776afd9c31f3d18294c3"
   },
   {
@@ -165,6 +188,7 @@ var speakers = [
     "github": "jweinstein",
     "twitter": "jeff_weinstein",
     "bio": "Jeff Weinstein is a data analytics engineer at Groupon working on monitoring and insight using real-time and large scale batch infrastructure. Previously, he was President at Hyperpublic, a local merchant data platform startup (acquired by Groupon), and prior led the R&D group at comScore analyzing lots of web logs. He hopes to humanize data analytics.",
+    "abstract": "",
     "hash": "3fd785240b555cdbb0d3465c00b79a30"
   },
   {
@@ -172,6 +196,7 @@ var speakers = [
     "github": "harrywincup",
     "twitter": "harrywincup",
     "bio": "Harry Wincup created the new Server Density UI and is responsible for the frontend engineering behind it, including building a custom backbone.js framework (before the likes of marionette existed). Recently he's been creating visualisations for large server deployments, health checking and incident management.",
+    "abstract": "",
     "hash": "e6f723dbccce896713eaaf7004944755"
   }
 ]
