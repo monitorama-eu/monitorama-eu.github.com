b3834bb18b7f051030e8b769b719193f5b56742d
obfuscurity <jason@dixongroup.net>
2018-03-31 15:50:07 -0400

    add sponsor opsview

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -6,4 +6,10 @@ var sponsors = [
     "width": 275,
     "bio": "<p></p>"
   } */
+  {
+    "name": "opsview",
+    "enabled": true,
+    "url": "https://www.opsview.com/",
+    "bio": "<p>Opsview is passionately focused on IT monitoring that enables DevOps teams to deliver smarter business services, faster. We help them be successful by providing unified, actionable insights into modern IT; including hardware, OSs, and applications, on-premises and in any cloud. Our vision is to harness the potential of the team to create the future for IT Operations. We are trusted by global enterprises, government organizations and leading service providers including Cisco, Sony, Microsoft, Allianz, IBM, BT, PayPal, MIT, Yale, Cornell, University of Chicago, LSE, Comcast and Telefonica. For more information, visit <a href=\"https://www.opsview.com/\">www.opsview.com</a> and follow us <a href=\"https://twitter.com/opsview\">@Opsview</a>.</p>"
+  },
 ]
