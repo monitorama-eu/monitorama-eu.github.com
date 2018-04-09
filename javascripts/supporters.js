d20c219c9fe6757055ad0569ebc9122d617d97f0
obfuscurity <jason@dixongroup.net>
2018-04-09 09:34:01 -0400

    add sponsor sematext

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -12,4 +12,10 @@ var sponsors = [
     "url": "https://www.opsview.com/",
     "bio": "<p>Opsview is passionately focused on IT monitoring that enables DevOps teams to deliver smarter business services, faster. We help them be successful by providing unified, actionable insights into modern IT; including hardware, OSs, and applications, on-premises and in any cloud. Our vision is to harness the potential of the team to create the future for IT Operations. We are trusted by global enterprises, government organizations and leading service providers including Cisco, Sony, Microsoft, Allianz, IBM, BT, PayPal, MIT, Yale, Cornell, University of Chicago, LSE, Comcast and Telefonica. For more information, visit <a href=\"https://www.opsview.com/\">www.opsview.com</a> and follow us <a href=\"https://twitter.com/opsview\">@Opsview</a>.</p>"
   },
+  {
+    "name": "sematext",
+    "enabled": true,
+    "url": "https://sematext.com/",
+    "bio": "<p><a href=\"https://sematext.com/\">Sematext Group</a>, a global products and services company, offers <a href=\"https://sematext.com/infrastructure-monitoring/\">infrastructure monitoring</a>, <a href=\"https://sematext.com/application-monitoring/\">application performance monitoring</a>, and <a href=\"https://sematext.com/log-management/\">log management</a> solutions - <a href=\"https://sematext.com/cloud\">Sematext Cloud</a> and Sematext Enterprise. Sematext enables businesses to easily collect application and server logs, metrics, traces, and events, thus delivering full-stack visibility through a single access point. The Sematext platform helps businesses extract value from logs and metrics and turn it into real-time actionable insights.</p><p>Sematext also provides professional services for Elasticsearch, the ELK / Elastic Stack, and Apache Solr – consulting, training, and production support.</p>"
+  },
 ]
