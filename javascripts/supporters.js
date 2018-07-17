84f769736072670e01c8730a2ff05bab3aebb8e7
obfuscurity <jason@dixongroup.net>
2018-07-17 09:29:38 -0400

    stage sponsors, add centreon

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -7,6 +7,19 @@ var sponsors = [
     "bio": "<p></p>"
   } */
   {
+    "name": "datadog",
+    "enabled": false,
+    "url": "http://www.datadoghq.com/",
+    "width": 150,
+    "bio": "<p>Datadog is a monitoring and analytics platform for large-scale application infrastructure. Combining metrics from servers, databases, and applications, Datadog delivers sophisticated, actionable alerts, and provides real-time visibility of your entire infrastructure. Datadog includes 120+ vendor-supported, prebuilt integrations and monitors hundreds of thousands of hosts.</p>"
+  },
+  {
+    "name": "wavefront",
+    "enabled": false,
+    "url": "https://www.wavefront.com/",
+    "bio": "<p>Wavefront™ by VMware® is a SaaS-based metrics monitoring and analytics platform that handles the high-scale requirements of modern cloud-native applications. Wavefront’s speed, scale and flexibility allows DevOps and developer teams instant insight into the performance of their highly-distributed cloud-native services. Wavefront’s analytics, query-driven alerts, interactive visualizations, open API, and integrations, all powered by massively scalable time-series database delivers “the first pane of glass” visibility helping DevOps teams detect performance anomalies while ensuring high availability of key cloud services.</p><p>Developers can self-serve and adapt Wavefront analytics to unique needs of their code while gaining visibility into its production behavior.</p>",
+  },
+  {
     "name": "opsview",
     "enabled": true,
     "url": "https://www.opsview.com/",
@@ -17,5 +30,25 @@ var sponsors = [
     "enabled": true,
     "url": "https://sematext.com/",
     "bio": "<p><a href=\"https://sematext.com/\">Sematext Group</a>, a global products and services company, offers <a href=\"https://sematext.com/infrastructure-monitoring/\">infrastructure monitoring</a>, <a href=\"https://sematext.com/application-monitoring/\">application performance monitoring</a>, and <a href=\"https://sematext.com/log-management/\">log management</a> solutions - <a href=\"https://sematext.com/cloud\">Sematext Cloud</a> and Sematext Enterprise. Sematext enables businesses to easily collect application and server logs, metrics, traces, and events, thus delivering full-stack visibility through a single access point. The Sematext platform helps businesses extract value from logs and metrics and turn it into real-time actionable insights.</p><p>Sematext also provides professional services for Elasticsearch, the ELK / Elastic Stack, and Apache Solr – consulting, training, and production support.</p>"
+  },
+  {
+    "name": "centreon",
+    "enabled": true,
+    "url": "https://www.centreon.com/",
+    "width": 225,
+    "bio": "<p>Centreon delivers open and flexible IT monitoring solutions that generate round-the-clock availability and performance insights of enterprise hybrid IT infrastructure: systems, networks and applications. Better yet, in a challenging period of digital transformation, it enables ITOM &amp; IT leaders to consolidate monitoring management of old and new systems while preparing for growing demands in ITSM, IT automation and AIOps.</p><p>Over 200,000 ITOM users worldwide within a growing community rely on Centreon software for strategic IT insights. With Centreon, ITOM professionals are on top of their game, pre-empting outages and improving overall performance, today and tomorrow.</p><p>For more information, visit <a href=\"https://www.centreon.com/en/\">www.centreon.com</a> and follow us on <a href=\"https://twitter.com/Centreon\">@Centreon</a>.</p>"
+  },
+  {
+    "name": "signalfx",
+    "enabled": false,
+    "url": "https://signalfx.com/",
+    "width": 175,
+    "bio": "<p>SignalFx is a leader in real-time operational intelligence for data-driven DevOps. The service discovers and collects metrics across every component in the cloud, replacing traditional point tools and providing real-time visibility into today’s dynamic environments. Leveraging the massively scalable SignalFx platform, the SaaS service is optimized for container and microservices based architectures and provides powerful visualization, proactive alerting, and collaborative triage capabilities across organizations of all sizes. SignalFx is used by Fortune 500 enterprises across financial services, apparel, industrials, telecommunications, media, and by web-scale players like Yelp, Hubspot, Acquia, and Kayak.</p>",
+  },
+  {
+    "name": "livemon",
+    "enabled": false,
+    "url": "https://signalfx.com/",
+    "bio": "<p></p>"
   },
 ]
